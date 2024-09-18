2ccda2f4808a66f5371e9033a2e9f94a24522a88
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-18 19:55:01 -0300

    add new page explore and create components for the explore page

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,10 @@
+export default function Explore() {
+    return (
+        <div>
+            {/* navbar */}
+            {/* sidebar */}
+            {/* main */}
+            {/* footer */}
+        </div>
+    )
+}
